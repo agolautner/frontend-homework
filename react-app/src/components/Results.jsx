7bdb349ebaabda700fb64cfe55f855b2ca3b73ab
agolautner <lautner.agoston@gmail.com>
2022-08-02 20:55:55 +0200

    minor fix with header display

--- react-app/src/components/Results.jsx
+++ react-app/src/components/Results.jsx
@@ -6,7 +6,7 @@ const Results = ({data, setter}) => {
   return (
         <div className='results'>
         Your size is  
-            {!data.length && <div className='results-data'>not possible to determine. Maybe try measuring again?</div>}
+            {!data.length && <div className='results-data'>not found.</div>}
             {data.length === 2 && <div className='results-data'>{data[1].label} or {data[0].label}</div>}
             {/* {data.length && data.map((item, index) => {
                 return (<div key={index}>{item.label}</div>)
