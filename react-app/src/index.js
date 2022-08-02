dbb635b44c348a7e04c2e3165b630538aeba2772
agolautner <lautner.agoston@gmail.com>
2022-08-02 21:07:43 +0200

    renamed app

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App3 from './App3';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App3 />
+    <App />
 );
